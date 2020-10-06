74932084af0f9f359fe5d118b707ba4f1296762f
Nidhi Rajendra <nidhirajendra@MacBook-Air.local>
2020-10-06 12:17:03 +0530

    typo

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install python3.6
-sudo apt install python3-pip
-echo "alias python=python3" >> ~/.bash_aliases
-source ~/.bash_aliases
-source ~/.bashrc
-echo "alias pip=pip3" >> ~/.bash_aliases
-source ~/.bash_aliases
+echo "Hello World"
