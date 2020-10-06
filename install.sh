ca1b7298431e2fe91fcd0ba156ab3681473551d1
Nidhi Rajendra <nidhirajendra@MacBook-Air.local>
2020-10-06 12:08:14 +0530

    alias python=python3

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install python3.6
+sudo apt install python3-pip
+echo "alias python=python3" >> ~/.bash_aliases
+source ~/.bash_aliases
+source ~/.bashrc
+echo "alias pip=pip3" >> ~/.bash_aliases
+source ~/.bash_aliases
