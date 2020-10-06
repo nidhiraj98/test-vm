0cba18057cf6a25cabb0fc56b653e23207ab8019
Nidhi Rajendra <nidhirajendra@MacBook-Air.local>
2020-10-06 09:26:23 +0530

    Running a custom script

---
+++ script.sh
@@ -0,0 +1,14 @@
+echo "Enter the Resource Group name:" &&
+read resourceGroupName &&
+echo "Enter the location (i.e. centralus):" &&
+read location &&
+echo "Enter the project name (used for generating resource names):" &&
+read projectName &&
+echo "Enter the administrator username:" &&
+read username &&
+echo "Enter the SSH public key:" &&
+read key &&
+# echo resourceGroupName
+az group create --name $resourceGroupName --location "$location" &&
+az deployment group create --resource-group $resourceGroupName --template-file template.json --parameters projectName=$projectName adminUsername=$username adminPublicKey="$(cat $key)" &&
+az vm show --resource-group $resourceGroupName --name "$projectName-vm" --show-details --query publicIps --output tsv
